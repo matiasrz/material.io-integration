32078cdd1223876d1294c4b8f3ef1bce9449947f
matiasrz <87562205+matiasrz@users.noreply.github.com>
2021-08-14 13:52:58 +0200

    Initial Commit & Base configurations - [New] Dependencies:   * React   * Webpack - [New] Config:   * Initial settings to run react & weback   * Babel

---
+++ README.md
@@ -0,0 +1,14 @@
+# GUIDE
+https://medium.com/mtholla/react-app-from-scratch-in-5-steps-e72d82034d5a
+
+# How this proyect was created from scracth.
+## 1. Basic functionalities
+npm i -D @babel/core @babel/cli @babel/preset-env @babel/preset-react
+npm i -D webpack webpack-cli webpack-dev-server babel-loader
+npm i -D browser-sync browser-sync-webpack-plugin
+npm i -s react react-dom
+
+## 2. Material Design Integration
+npm i -D css-loader sass-loader sass extract-loader file-loader mini-css-extract-plugin
+npm i -D autoprefixer postcss-loader url-loader
+npm i -D @material/drawer @material/top-app-bar @material/shape @material/density
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const webpack = require('webpack');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ["@babel/preset-env"] },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            // options: {
+            //   name: 'bundle.css'
+            // },
+          },
+          { loader: 'extract-loader' },
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: { 
+                plugins: ["autoprefixer"],
+              },
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                includePaths: ['./node_modules'],
+              },
+              // Prefer Dart Sass
+              implementation: require('sass'),
+
+              // See https://github.com/webpack-contrib/sass-loader/issues/804
+              webpackImporter: true,              
+            },
+          },
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      // Images: path.resolve(__dirname, 'src/assets/images/'),
+      // MDComponents: path.resolve(__dirname, 'src/components/MaterialDesign/')
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3001,
+    publicPath: 'http://localhost:3001/dist',
+    hotOnly: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 8080,
+        proxy: 'http://localhost:3002/',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[name].css'
+    }),
+  ],
+  optimization: {
+    minimize: true
+  },
+};
